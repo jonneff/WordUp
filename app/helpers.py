814fa4012c12b8533774060fd232d2af3ac1df2c
jonneff <jon.m.neff@gmail.com>
2016-02-26 20:53:28 -0800

    Tested and fixed regexes

--- app/helpers.py
+++ app/helpers.py
@@ -7,9 +7,10 @@ Functions in this module:
 """
 
 import re
+import unicodedata
 
 class WordUp(object): 
-    def cleanup(body):
+    def cleanup(self, body):
         """
         use regular expressions to clean up comment text, e.g., punctuation etc.
         :type body:  unicode str
@@ -19,19 +20,21 @@ class WordUp(object):
         body = re.sub("&lt;", "<", body)
         body = re.sub("&amp;", "&", body)
         body = re.sub("&nbsp;", " ", body)
-        body = re.sub("^[deleted]$", "", body) # Remove deleted
-        body = re.sub("http[[:alnum:][:punct:]]*", " ", body) # Remove URL
-        body = re.sub("/r/[[:alnum:]]+|/u/[[:alnum:]]+", " ", body) # Remove /r/subreddit, /u/user
+        body = re.sub("^\[deleted\]$", "", body) # Remove deleted
+        body = re.sub("http\S+", " ", body) # Remove URL
+        body = re.sub("/r/\S+|/u/\S+", " ", body) # Remove /r/subreddit, /u/user
         # body = re.sub("(>.*?\\n\\n)+", " ", body) # Remove quoted comments
-        body = re.sub("[[:cntrl:]]", " ", body) # Remove control characters (\n, \b)
+        # body = re.sub("[[:cntrl:]]", " ", body) # Remove control characters (\n, \b) doesn't work for unicode
+        body = "".join(ch for ch in body if unicodedata.category(ch)[0]!="C") # Remove control characters (\n, \b) etc.
         body = re.sub("'", "", body) # Remove single quotation marks (contractions)
-        body = re.sub("[[:punct:]]", " ", body) # Remove punctuation
+        # body = re.sub("[[:punct:]]", " ", body) # Remove punctuation
+        body = "".join(ch for ch in body if unicodedata.category(ch)[0]!="P") # Remove punctuation
         body = re.sub("\\s+", " ", body) # Replace multiple spaces with single space
         body = body.strip()
         body = body.lower() # Lower case
         return body # Return body (cleaned up text)
 
-    def label(score):
+    def label(self, score):
         if int(score) <= -1: 
             return 'neg'
         else: 
--- test/unit/helpers_test.py
+++ test/unit/helpers_test.py
@@ -3,35 +3,73 @@
 import sys
 import unittest
 import unicodecsv
+# import csv
 from app.helpers import WordUp
 
 class TestWordUp(unittest.TestCase): 	
     # CONSIDER ADDING PYTEST FIXTURES FOR CONSTANTS 
     def setUp( self): 
         self.w = WordUp() 
-        self.f = open('data/test.csv', 'rb')
-        self.reader = unicodecsv.reader(f, encoding='utf-8')
 
-    def test_cleanup_handles_gt(self):
-        row = next(self.reader)
-        body = map(lambda line: line[0], row)
+    def test_html_codes(self):
+        body = u'&gt;her kids have to pay the debt i was stuck making &lt;$55k to ship &amp; drive day to&nbsp;day'
         actual = self.w.cleanup(body)
-        expected = u'
-        self.assertIsNone(result)
+        expected = u'>her kids have to pay the debt i was stuck making <$55k to ship drive day to day'
+        self.assertEqual(expected, actual)
 
-    def test_parse_line_method_returns_correct_result(self):
-        line = self.goodlog.readline()
-        actual = self.parse.parse_line(line) 
-        expected = [1389721010,'/svds.com','http://www.svds.com/rockandroll/','198.0.200.105','SILICON VALLEY DATA SCIENC', 
-                    37.8858, -122.118, 'Comcast Business Communications, LLC']
+    def test_remove_deleted(self):
+        body = u'[deleted]'
+        actual = self.w.cleanup(body)
+        expected = u''
         self.assertEqual(expected, actual)
         
+    def test_remove_url(self):
+        body = u'test url https://www.reddit.com/r/investing/comments/3pulwr/i_just_won_a_25000_lawsuit_what_do_i_do/'
+        actual = self.w.cleanup(body)
+        expected = u'test url'
+        self.assertEqual(expected, actual)
+        
+    def test_remove_subreddit_user(self):
+        body = u'test remove reddit slashes /r/personalfinance /u/fakeuser'
+        actual = self.w.cleanup(body)
+        expected = u'test remove reddit slashes'
+        self.assertEqual(expected, actual)
+    
+    def test_remove_control_chars(self):
+        body = u'remove control chars \n they would drop you in a \b second'
+        actual = self.w.cleanup(body)
+        expected = u'remove control chars they would drop you in a second'
+        self.assertEqual(expected, actual)
+        
+    def test_remove_single_quotes(self):
+        body = u"remove single quotes it's in it's interest"
+        actual = self.w.cleanup(body)
+        expected = u'remove single quotes its in its interest'
+        self.assertEqual(expected, actual)
+    
+    def test_remove_punctuation(self):
+        body = u'test punctuation!  no really, bullfrogs? such is life:  it is.'
+        actual = self.w.cleanup(body)
+        expected = u'test punctuation no really bullfrogs such is life it is'
+        self.assertEqual(expected, actual) 
+        
+    def test_remove_multiple_spaces(self):
+        body = u'  leading and multiple     spaces       should     be       removed   '
+        actual = self.w.cleanup(body)
+        expected = u'leading and multiple spaces should be removed'
+        self.assertEqual(expected, actual)
+        
+    def test_lower_case(self):
+        body = u'THIS IS ALL UPPERCASE BUT SHOULD BE LOWER'
+        actual = self.w.cleanup(body)
+        expected = u'this is all uppercase but should be lower'
+        self.assertEqual(expected, actual)
 
-    def __del__(self):
+    # def __del__(self):
         # close files in destructor method
         # destructors are controversial in Python but while seems awkward in this case
         # IMPORTANT:  avoid circular references with other classes when using destructor.
-        self.f.close()
+        # self.f.close()
 
 if __name__ == '__main__':
     unittest.main()
